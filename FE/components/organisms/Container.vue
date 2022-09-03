21a06f3825ff16e580553bc6b15722396b6b6076
tulio <tulioam9@gmail.com>
2022-09-03 05:53:24 -0300

    fix(FE): remove padding from Header

--- FE/components/organisms/Container.vue
+++ FE/components/organisms/Container.vue
@@ -1,5 +1,5 @@
 <template>
-    <article class="p-4 max-w-[1440px] mx-auto">
+    <article class="px-4 pb-4 max-w-[1440px] mx-auto">
         <slot></slot>
     </article>
 </template>
--- FE/components/organisms/Header.vue
+++ FE/components/organisms/Header.vue
@@ -1,12 +1,17 @@
 <template>
-  <header class="flex p-4 justify-between">
+  <header class="flex my-4 justify-between">
     <div>
       <h1 class="text-3xl md:text-4xl">
         YET ANOTHER <span class="text-[#f0db4f]">JS</span> FRAMEWORK...
       </h1>
       <h3>Making sense of the Javascript ecosystem</h3>
     </div>
-    <a href="https://github.com/TulioAlcantara/yet-another-js-framework" target="_blank" class="hidden md:block text-white hover:text-[#f0db4f] underline">Github</a>
+    <a
+      href="https://github.com/TulioAlcantara/yet-another-js-framework"
+      target="_blank"
+      class="hidden md:block text-white hover:text-[#f0db4f] underline"
+      >Github</a
+    >
   </header>
 </template>
 
