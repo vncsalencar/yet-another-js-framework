3af76d69afbb72a51e8f7bc8ace5d5d928711019
Vinícius Borges Alencar <viniciusale4@gmail.com>
2022-09-08 14:35:26 -0300

    refactor(BE): try to use `port` from .env

--- BE/src/main.rs
+++ BE/src/main.rs
@@ -9,13 +9,19 @@ use dotenv::dotenv;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    let port = std::env::var("BE_PORT")
+        .unwrap_or_else(|_| "8088".to_string())
+        .parse::<u16>()
+        .expect("PORT must be a number");
+
+    dbg!("Starting server on port {port}");
 
     HttpServer::new(|| {
         App::new()
             .route("/ping", web::get().to(|| async { "pong" }))
             .configure(build_api)
     })
-    .bind(("0.0.0.0", 8088))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
