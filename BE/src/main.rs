a607b3bcff38e9f49173ad43bd8226451df0fc23
Vinícius Borges Alencar <viniciusale4@gmail.com>
2022-09-08 18:17:57 -0300

    chore(BE): add `Logger` middleware to server

--- BE/src/main.rs
+++ BE/src/main.rs
@@ -2,13 +2,14 @@ mod api;
 mod api_state;
 mod strapi;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use api::build_api;
 use dotenv::dotenv;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let port = std::env::var("BE_PORT")
         .unwrap_or_else(|_| "8088".to_string())
         .parse::<u16>()
@@ -18,6 +19,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .route("/ping", web::get().to(|| async { "pong" }))
             .configure(build_api)
     })
