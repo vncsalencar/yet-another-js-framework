02860ec170251a7055aab7757973a0abbf4e8156
tulio <tulioam9@gmail.com>
2022-09-13 01:40:26 -0300

    feat(FE): add "escape" listener to toggle menu

--- FE/app.vue
+++ FE/app.vue
@@ -13,6 +13,10 @@
 </template>
 
 <script setup lang="ts">
+import { useMenuStore } from "./stores/menuStore";
+
+const store = useMenuStore();
+
 useHead({
   title: "Yet another Javascript framework",
   htmlAttrs: {
@@ -20,11 +24,22 @@ useHead({
   },
 });
 
+onMounted(() => {
+  document.onkeydown = (ev: KeyboardEvent) => {
+    if (ev.key == "Escape") {
+      store.toggle();
+    }
+  };
+});
+
 const scrollTo = (sectionId: SectionIds) => {
+  if (sectionId == "home") {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    return;
+  }
+
   const headerHeight = document.getElementById("header").offsetHeight + 16;
   const position = document.getElementById(sectionId).offsetTop - headerHeight;
   window.scrollTo({ top: position, behavior: "smooth" });
 };
-
-onMounted(() => {});
 </script>
