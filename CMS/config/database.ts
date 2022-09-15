3f3083412da66af9cff275e2654848e53cb133c6
Vinícius Borges Alencar <viniciusale4@gmail.com>
2022-09-15 18:39:46 -0300

    feat(CMS): use postgres database

--- CMS/config/database.ts
+++ CMS/config/database.ts
@@ -1,11 +1,22 @@
-import path from 'path';
-
 export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+    // client: 'sqlite',
+    // connection: {
+    //   filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    // },
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', 'postgres'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
     useNullAsDefault: true,
-  },
 });
